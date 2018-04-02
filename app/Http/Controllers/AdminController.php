417edb2ba3ab08cc74f527c547d074a95faf086e
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-02 13:29:29 -0700

    Adjust Admin controller.
     Generate unique, 12 digit pw.
    Create user account, and then assosiate a new admin account with it.
    
    Create logic for password setting

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Admin;
+use App\User;
 use Illuminate\Http\Request;
 use Validator;
 use Redirect;
 
 use Illuminate\Support\Facades\Mail;
 use App\Mail\AdminCreated;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class AdminController extends Controller
 {
@@ -19,7 +24,7 @@ class AdminController extends Controller
      */
     public function index()
     {
-        $admins = Admin::get();
+        $admins = Admin::with('user')->get();
         return view('admin.admins.index',
             [
                 'admins' => $admins,
@@ -58,17 +63,24 @@ class AdminController extends Controller
             return redirect()->back()->withErrors($validator)->withInput($request->all());
         }
 
+        $pwReturn = str_random(12);
 
-        $pwReturn = md5(microtime());
+
+        $user = new User();
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->org = "Admin";
+            $user->email = $request->email;
+            $user->name = $request->email;
+            $user->password = Hash::make($pwReturn);
+            $user->active = true;
+            $user->save();
 
         $admin = new Admin();
-            $admin->first_name = $request->first_name;
-            $admin->last_name = $request->last_name;
-            $admin->email = $request->email;
-            $admin->password = $pwReturn;
+            $admin->user_id = $user->id;
             $admin->save();
 
-        Mail::to($request->email)->send(new AdminCreated($admin));
+        Mail::to($request->email)->send(new AdminCreated($user, $pwReturn));
 
         return $pwReturn;
     }
@@ -116,5 +128,52 @@ class AdminController extends Controller
     public function destroy(Admin $admin)
     {
         //
+    }
+
+    public function password() {
+        $admin = Auth::user();
+
+        if (Auth::check()) {
+            return view('admin.admins.password',
+                [
+                    'admin' => $admin,
+                ]
+            );
+        }
+        return redirect('/login');
+    }
+
+    public function passwordSave(Request $request) {
+        $admin = Auth::user();
+
+        if (Auth::check()) {
+            $rules = array(
+                'password' => 'required|string|min:6|confirmed',
+                'user_id' => 'required|exists:users,id',
+            );
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput($request->all());
+            }
+
+            if($request->user_id == Auth::id()) {
+                $user = User::find($request->user_id)->first();
+                if($user != null) {
+                    $user->password = $user->password = Hash::make($request->password);
+                    $user->save();
+
+                    $admin = Admin::where('user_id', '=', $request->user_id)->first();
+                        $admin->active = true;
+                        $admin->save();
+                    return redirect('/admin');
+                }
+            }
+
+            return redirect()->back()->withErrors('Failed');
+
+        }
+        return redirect('/login');
     }
 }
