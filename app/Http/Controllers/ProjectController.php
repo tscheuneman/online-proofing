a696eae2adbfdbeffd47ee20755c88d2051ab835
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-03 12:52:22 -0700

    Create logic to create project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Project;
 use App\Categories;
+use App\Admin;
+use App\User;
+use App\UserAssign;
+use App\AdminAssign;
+
 use Illuminate\Http\Request;
+
+use Validator;
+use Redirect;
+use Storage;
 
 class ProjectController extends Controller
 {
@@ -47,7 +56,92 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = array(
+            'name' => 'required|string',
+            'category' => 'required|exists:categories,id',
+            'adminValues' => 'required|json',
+            'userValues' => 'required|json'
+        );
+        $hidden = false;
+        $notify_users = false;
+        $notify_admins = false;
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput($request->all());
+        }
+        if($request->hidden) {
+            $hidden = true;
+        }
+        if($request->notify_users) {
+            $notify_users = true;
+        }
+        if($request->notify_admins) {
+            $notify_admins = true;
+        }
+        $admins = json_decode($request->adminValues);
+        $users = json_decode($request->userValues);
+
+        $errors = false;
+        $errorArray = array();
+
+        foreach($admins as $admin) {
+            $testAdmin = Admin::find($admin);
+            if($testAdmin == null) {
+                $errorArray[] = 'Invalid Premedia Member';
+                $errors = true;
+                break;
+            }
+        }
+        foreach($users as $user) {
+            $testUser = User::find($user);
+            if($testUser == null) {
+                $errorArray[] = 'Invalid Customer';
+                $errors = true;
+                break;
+            }
+        }
+
+        if($errors) {
+            return redirect()->back()->withErrors($errorArray)->withInput($request->all());
+        }
+
+        $path = date('Y') . '/' . date('F') . '/' . md5(microtime());
+
+        if(Storage::disk('local')->makeDirectory('projects/' . $path)) {
+
+            $project = new Project();
+                $project->project_name = $request->name;
+                $project->file_path = $path;
+                $project->cat_id = $request->category;
+                $project->hidden = $hidden;
+                $project->notify_users = $notify_users;
+                $project->notify_admins = $notify_admins;
+                $project->save();
+
+            foreach($users as $user) {
+                $assign = new UserAssign();
+                    $assign->user_id = $user;
+                    $assign->project_id = $project->id;
+                    $assign->notify = $notify_users;
+                    $assign->save();
+            }
+            foreach($admins as $admin) {
+                $assign = new AdminAssign();
+                $assign->user_id = $admin;
+                $assign->project_id = $project->id;
+                $assign->notify = $notify_admins;
+                $assign->save();
+            }
+
+            return "it worked";
+        }
+
+
+        return $admins;
+
+
     }
 
     /**
@@ -58,7 +152,7 @@ class ProjectController extends Controller
      */
     public function show(Project $project)
     {
-        //
+
     }
 
     /**
