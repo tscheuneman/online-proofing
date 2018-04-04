748b28b316dc5e08f0a31747284b2a9665ad32e1
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-04 10:27:59 -0700

    Adjust password change views.

--- resources/views/admin/admins/password.blade.php
+++ resources/views/admin/admins/password.blade.php
@@ -9,29 +9,36 @@
     @if(Session::has('flash_created'))
         <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_created') !!}</em></div>
     @endif
-    <h2>
-        Hi {{$admin->first_name}}, set your password
-    </h2>
-    <br>
-    <form method="POST" action="{{ url('/admin/password') }}" enctype="multipart/form-data" id="submit">
-        {{csrf_field()}}
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Hi {{$admin->first_name}}, set your password
+                    </h2>
+                    <br>
+                    <form method="POST" action="{{ url('/admin/password') }}" enctype="multipart/form-data" id="submit">
+                        {{csrf_field()}}
 
-        <input type="hidden" name="user_id" value="{{$admin->id}}">
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="hidden" name="user_id" value="{{$admin->id}}">
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                        </div>
+
+                        <hr>
+
+                        <button type="submit" class="btn btn-submission">Submit</button>
+                        <br>
+                        @include('layouts.errors')
+                    </form>
+                </div>
+            </div>
         </div>
-
-        <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-        </div>
-
-        <hr>
-
-        <button type="submit" class="btn btn-submission">Submit</button>
-        <br>
-        @include('layouts.errors')
-
-    </form>
+    </div>
 @endsection
--- resources/views/main/users/password.blade.php
+++ resources/views/main/users/password.blade.php
@@ -9,29 +9,37 @@
     @if(Session::has('flash_created'))
         <div class="alert alert-success"><span class="glyphicon glyphicon-ok"></span><em> {!! session('flash_created') !!}</em></div>
     @endif
-    <h2>
-        Hi {{$user->first_name}}, set your password
-    </h2>
-    <br>
-    <form method="POST" action="{{ url('/password') }}" enctype="multipart/form-data" id="submit">
-        {{csrf_field()}}
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <h2>
+                        Hi {{$user->first_name}}, set your password
+                    </h2>
+                    <br>
+                    <form method="POST" action="{{ url('/password') }}" enctype="multipart/form-data" id="submit">
+                        {{csrf_field()}}
 
-        <input type="hidden" name="user_id" value="{{$user->id}}">
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="hidden" name="user_id" value="{{$user->id}}">
+                        <div class="form-group">
+                            <label for="password">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password_confirmation">Confirm Password</label>
+                            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                        </div>
+
+                        <hr>
+
+                        <button type="submit" class="btn btn-submission">Submit</button>
+                        <br>
+                        @include('layouts.errors')
+                    </form>
+                </div>
+            </div>
         </div>
+    </div>
 
-        <div class="form-group">
-            <label for="password_confirmation">Confirm Password</label>
-            <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
-        </div>
-
-        <hr>
-
-        <button type="submit" class="btn btn-submission">Submit</button>
-        <br>
-        @include('layouts.errors')
-
-    </form>
 @endsection
