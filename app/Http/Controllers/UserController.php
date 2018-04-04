cfa3f885ebc920f5ead8ccf7ddcc370afa851ff5
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-04 09:42:26 -0700

    Create logic for users.
    The creation of new users
    
    Logic to change user passwords

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,14 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserCreated;
+
+use Validator;
+
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -29,7 +37,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.users.create');
     }
 
     /**
@@ -40,7 +48,35 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = array(
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'org' => 'required|string'
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput($request->all());
+        }
+
+        $pwReturn = str_random(12);
+
+        $user = new User();
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->org = $request->org;
+            $user->email = $request->email;
+            $user->name = $request->email;
+            $user->password = Hash::make($pwReturn);
+            $user->active = false;
+            $user->save();
+
+        Mail::to($request->email)->send(new UserCreated($user, $pwReturn));
+
+        \Session::flash('flash_created','Account for' . $user->first_name . ' has been created');
+        return redirect('/admin/customers');
     }
 
     /**
@@ -87,4 +123,56 @@ class UserController extends Controller
     {
         //
     }
+
+    public function password() {
+        $user = Auth::user();
+        if(!$user->active) {
+            if (Auth::check()) {
+                return view('main.users.password',
+                    [
+                        'user' => $user,
+                    ]
+                );
+            }
+            return redirect('/login');
+        }
+        return redirect('/admin');
+    }
+
+    public function passwordSave(Request $request)
+    {
+        $theUser = Auth::user();
+        if (!$theUser->active) {
+            if (Auth::check()) {
+                $rules = array(
+                    'password' => 'required|string|min:6|confirmed',
+                    'user_id' => 'required|exists:users,id',
+                );
+
+                $validator = Validator::make($request->all(), $rules);
+
+                if ($validator->fails()) {
+                    return redirect()->back()->withErrors($validator)->withInput($request->all());
+                }
+
+                if ($request->user_id == Auth::id()) {
+                    $user = User::find($request->user_id);
+                    if ($user != null) {
+                        $user->password = Hash::make($request->password);
+                        $user->active = true;
+                        $user->save();
+
+                        return redirect('/');
+                    }
+                }
+
+                return redirect()->back()->withErrors('Failed');
+
+            }
+            return redirect('/login');
+        }
+        return redirect('/');
+    }
+
+
 }
