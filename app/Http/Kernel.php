be5ce6a2350c4103e28b30b5a0090785591af76e
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-04 09:42:32 -0700

    Add User middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -60,5 +60,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'admin' => \App\Http\Middleware\CheckAdmin::class,
+        'user' => \App\Http\Middleware\CheckUser::class,
     ];
 }
