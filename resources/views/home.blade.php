339ddcd64f131bc11f7f254dbca3cf21fd5c84f0
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-04 09:43:48 -0700

    Home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-3">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">User Info</div>
+
+                <div class="card-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">Active Projects</div>
 
                 <div class="card-body">
                     @if (session('status'))
