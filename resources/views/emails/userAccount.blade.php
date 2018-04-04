d37b1f8e702d451c97537579f2c071d06d8ac730
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-04 09:43:41 -0700

    New user account html email view

---
+++ resources/views/emails/userAccount.blade.php
@@ -0,0 +1,46 @@
+@include('emails.components.header')
+<table border="0" cellpadding="0" cellspacing="0" class="body">
+    <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content">
+
+                <!-- START CENTERED WHITE CONTAINER -->
+                <span class="preheader">Thank you for registering</span>
+                <table class="main">
+
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <td class="wrapper">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td>
+                                        <h1>Hi {{$user->first_name}}</h1>
+                                        <p>You have been registered as a new customer at the Print and Imaging Lab's proofing system</p>
+                                        <p>Your email is: <strong>{{$user->email}}</strong></p>
+                                        <p>Here is your inital password: <strong>{{$pw}}</strong></p>
+                                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                                            <tbody>
+                                            <tr>
+                                                <td align="left">
+                                                    <table border="0" cellpadding="0" cellspacing="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td> <a href="{{$redirect}}" target="_blank">Login</a> </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <p>Once you login in with your temporary password. You will be prompted to change it to a password of your choice</p>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+@include('emails.components.footer')
