4d7daae36be27c3ea240f803db56bfbaf85cd48d
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-06 16:04:24 -0700

    Change relation, get entry no matter what

--- app/Project.php
+++ app/Project.php
@@ -24,7 +24,7 @@ class Project extends Model
         return $this->hasMany('App\AdminAssign', 'project_id','id')->select('id', 'user_id', 'project_id');
     }
     public function admin_entries() {
-        return $this->hasMany('App\Entry', 'project_id','id')->select('id', 'project_id', 'admin', 'active')->where('active', '=', true)->latest();
+        return $this->hasMany('App\Entry', 'project_id','id')->select('id', 'project_id', 'admin', 'active')->latest();
     }
     public function entries() {
         return $this->hasMany('App\Entry', 'project_id','id')->where('active', '=', true)->latest();
