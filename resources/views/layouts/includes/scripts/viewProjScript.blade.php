4bb0f60dbac006e0f68180b35b50b4b49e967931
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-10 12:08:18 -0700

    Adjust

--- resources/views/layouts/includes/scripts/viewProjScript.blade.php
+++ resources/views/layouts/includes/scripts/viewProjScript.blade.php
@@ -2,10 +2,10 @@
     $(document).ready(function() {
         updatePageCount();
         $('#next').on('click', function() {
-            goNext();
+            goNextItemProjectView();
         });
         $('#prev').on('click', function() {
-            goPrev();
+            goPreviousItemProjectView();
         });
         $('.entryNav').on('click', function() {
             let activeElm = $('div.entry.active').data('id');
@@ -13,56 +13,7 @@
             if(activeElm === thisElm) {
                 return false;
             }
-            alert(activeElm);
+            goToEntryItemById(thisElm);
         });
     });
-    function goNext() {
-        let currEntry = $('div.entry.active');
-        let maxElm = currEntry.data('numelm');
-        let currImg = $('div.image.active', currEntry);
-
-        let currImgNum = currImg.data('num');
-        let nextImg = currImgNum + 1;
-        if(parseInt(nextImg) >= parseInt(maxElm)) {
-            nextImg = 0;
-        }
-
-        currImg.fadeOut(200, function() {
-            $(this).removeClass('active');
-            $('div.image.proj_' + nextImg, currEntry).fadeIn(200, function() {
-                $(this).addClass('active');
-                updatePageCount();
-            });
-        });
-    }
-
-    function goPrev() {
-        let currEntry = $('div.entry.active');
-        let maxElm = parseInt(currEntry.data('numelm')) - 1;
-        let currImg = $('div.image.active', currEntry);
-
-        let currImgNum = currImg.data('num');
-        let nextImg = currImgNum - 1;
-        if(parseInt(nextImg) < 0) {
-            nextImg = maxElm;
-        }
-
-        currImg.fadeOut(200, function() {
-            $(this).removeClass('active');
-            $('div.image.proj_' + nextImg, currEntry).fadeIn(200, function() {
-                $(this).addClass('active');
-                updatePageCount();
-            });
-        });
-    }
-
-    function updatePageCount() {
-        let currEntry = $('div.entry.active');
-        let maxElm = parseInt(currEntry.data('numelm'));
-        let currImg = $('div.image.active', currEntry);
-        let currImgNum = parseInt(currImg.data('num')) + 1;
-
-        $('div.counter span.current').html(currImgNum);
-        $('div.counter span.max').html(maxElm);
-    }
 </script>
