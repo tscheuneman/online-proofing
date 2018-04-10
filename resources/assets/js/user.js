a11cefec1ca61d421110a77e986b1fdbfb98be25
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-10 08:53:23 -0700

    JS

---
+++ resources/assets/js/user.js
@@ -0,0 +1,14 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.LC = require('literallycanvas');
+
+window.axios = require('axios');
+
+LC.init(document.getElementById('main'), {imageURLPrefix: "/build/lc-assets/img"});
