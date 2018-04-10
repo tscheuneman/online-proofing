b253723dcfea441019bbb0f0360f9cc55bb9337b
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-10 12:00:18 -0700

    Create user submission page

--- app/Http/Controllers/UserProjectController.php
+++ app/Http/Controllers/UserProjectController.php
@@ -4,6 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
+use App\Entry;
+
+use Illuminate\Support\Facades\Auth;
+use App\Jobs\UserEntry;
+
+use Mockery\Exception;
+
+use Validator;
+use File;
 
 class UserProjectController extends Controller
 {
@@ -35,7 +44,64 @@ class UserProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = array(
+            'dataArray' => 'required|json',
+            'projectID' => 'required|exists:projects,id'
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return "failure";
+        }
+        $rand = str_random(12);
+
+        $project = Project::find($request->projectID);
+
+        $proj_year = date('Y', strtotime($project->created_at));
+        $proj_month = date('F', strtotime($project->created_at));
+
+        $projectPath = $proj_year . '/' . $proj_month . '/' . $project->file_path;
+
+        if(File::makeDirectory(public_path('/storage/' . 'projects/' . $projectPath . '/' . $rand), 0777, true)) {
+            try {
+                $dir = 'projects/' . $projectPath . '/' . $rand;
+
+                $files = [];
+                $comments = [];
+
+                $data = json_decode($request->dataArray);
+
+                foreach($data as $val) {
+                    $files[] = $val->data;
+                    $comments[] = $val->comments;
+                }
+
+                $entry = new Entry();
+                $entry->project_id = $project->id;
+                $entry->user_id = Auth::id();
+                $entry->path = $rand;
+                $entry->pdf_name = null;
+                $entry->user_notes = null;
+                $entry->notes = null;
+                $entry->save();
+
+                UserEntry::dispatch($comments, $files, $entry, $dir);
+
+                $returnData['status'] = 'Success';
+                $returnData['message'] = 'Uploaded';
+                return json_encode($returnData);
+
+            } catch (Exception $e) {
+                return $e;
+            }
+
+
+        }
+
+        $returnData['status'] = 'Failure';
+        $returnData['message'] = 'Failed to upload';
+        return json_encode($returnData);
     }
 
     /**
@@ -53,7 +119,9 @@ class UserProjectController extends Controller
                 return view('main.project.index',
                     [
                         'project' => $thisProject,
-                    ]
+                        'val'=> json_encode($thisProject->entries[0], JSON_UNESCAPED_SLASHES ),
+
+                ]
                 );
                 return redirect()->back();
             }
