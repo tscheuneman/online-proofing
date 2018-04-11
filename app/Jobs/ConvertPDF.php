14c42a4132ee1606061c3c721993f6b620b2f9b9
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-11 10:24:03 -0700

    Test Conversion

--- app/Jobs/ConvertPDF.php
+++ app/Jobs/ConvertPDF.php
@@ -8,13 +8,16 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserNotify;
+
 use Storage;
 use File;
 use Imagick;
 
 use App\Entry;
 use App\Project;
-
+use App\UserAssign;
 
 use Mockery\Exception;
 
@@ -52,12 +55,16 @@ class ConvertPDF implements ShouldQueue
             if(File::makeDirectory( public_path('/storage/' . $this->dir . '/images'), 0777, true)) {
                 $savePath = public_path('/storage/' . $this->dir . '/images/');
                 try {
+
                     $im = new Imagick();
                     $files = array();
-                    $im->setResolution(300,300);
                     $im->readimage($realPath);
+                    $im->stripImage();
+                    $im->setType(6);
+                    $im->setImageType(6);
+                    $im->setColorspace(imagick::COLORSPACE_RGB);
                     $numPages = $im->getNumberImages();
-                    \Log::info($numPages);
+
                     for($x = 0; $x < $numPages; $x++) {
                         $num_padded = sprintf("%02d", $x);
                         $im->setIteratorIndex($x);
@@ -68,17 +75,26 @@ class ConvertPDF implements ShouldQueue
                         $files[$x]['width'] = 650;
                         $files[$x]['height'] = $d['height'];
                         $files[$x]['file'] = 'image_' . $num_padded . '.png';
-                        $files[$x]['comments'] = '';
                     }
                     $im->clear();
                     $im->destroy();
 
+                    $this->entry->delete();
+                    /*
                     $this->project->active = true;
                     $this->project->save();
 
                     $this->entry->active = true;
                     $this->entry->files = json_encode($files);
                     $this->entry->save();
+
+                    if($this->project->notify_users) {
+                        $users = UserAssign::with('user')->where('project_id', $this->project->id)->get();
+                        foreach($users as $user) {
+                            Mail::to($user->user->email)->send(new UserNotify($user->user->id, $this->project));
+                        }
+                    }
+            */
                 } catch(\Exception $e) {
                     report($e);
                 }
