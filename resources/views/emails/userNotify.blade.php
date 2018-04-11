6b390ce532a02e29d2953ccb8d6af845e7097532
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-11 11:08:37 -0700

    User notify email

---
+++ resources/views/emails/userNotify.blade.php
@@ -0,0 +1,44 @@
+@include('emails.components.header')
+<table border="0" cellpadding="0" cellspacing="0" class="body">
+    <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content">
+
+                <!-- START CENTERED WHITE CONTAINER -->
+                <span class="preheader">You have been added to a project</span>
+                <table class="main">
+
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <td class="wrapper">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td>
+                                        <h1>Hi {{$user->first_name}}</h1>
+                                        <p>You have been added as an appover to a project requiring proof approval!</p>
+                                        <p>Project: <strong>{{$project->project_name}}</strong></p>
+                                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                                            <tbody>
+                                            <tr>
+                                                <td align="left">
+                                                    <table border="0" cellpadding="0" cellspacing="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td> <a href="{{ENV('APP_URL')}}/project/6Ys8PiQbwyaB" target="_blank">Link to Project</a> </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+@include('emails.components.footer')
