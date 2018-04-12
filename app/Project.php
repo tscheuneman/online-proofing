c55ddd3ce1386e3dd9305f28b8a0dd6a0ba3144c
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-12 16:02:59 -0700

    Remove this logic in favor of orders

--- app/Project.php
+++ app/Project.php
@@ -11,22 +11,14 @@ class Project extends Model
 
     public $incrementing = false;
 
-    public function category()
-    {
-        return $this->hasOne('App\Categories', 'id','cat_id')->select('id', 'name');
-    }
-    public function users()
-    {
-        return $this->hasMany('App\UserAssign', 'project_id','id')->select('id', 'user_id', 'project_id');
-    }
-    public function admins()
-    {
-        return $this->hasMany('App\AdminAssign', 'project_id','id')->select('id', 'user_id', 'project_id');
-    }
     public function admin_entries() {
         return $this->hasMany('App\Entry', 'project_id','id')->select('id', 'project_id', 'admin', 'active', 'created_at')->latest();
     }
     public function entries() {
         return $this->hasMany('App\Entry', 'project_id','id')->where('active', '=', true)->latest();
+    }
+    public function order()
+    {
+        return $this->belongsTo('App\Order', 'ord_id', 'id');
     }
 }
