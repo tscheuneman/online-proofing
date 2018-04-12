feaa0eea2aea4648c6100f536a34be5bf7711e95
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-12 16:05:50 -0700

    Revamp projects page
    Show projects as children of orders

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -12,79 +12,60 @@
     <h2>
         Projects
     </h2>
-    <a class="btn btn-main" href="/admin/projects/create"><i class="fa fa-plus" aria-hidden="true"></i> Create Project</a>
+    <a class="btn btn-main" href="/admin/orders/create"><i class="fa fa-plus" aria-hidden="true"></i> Create Order</a>
     <a class="btn btn-second" href="/admin/categories/create"><i class="fa fa-plus" aria-hidden="true"></i> Create Category</a>
     <br class="clear" />
     <br />
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Project Name</th>
-            <th scope="col">Categories</th>
-            <th scope="col">Assigned Customers</th>
-            <th scope="col">Assigned PreMedia</th>
-            <th scope="col">Status</th>
-            <th scope="col">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        @php
-            $class = '';
-        @endphp
-        @foreach($projects as $proj)
-            @foreach($proj->admins as $admin)
-                @if($admin->admin->user_id == Auth::id())
-                    @php
-                        $class = 'belongs';
-                    @endphp
-                    @break
-                 @endif
-         @endforeach
-            <tr class="{{$class}}">
-                <td>{{$proj->project_name}}</td>
-                <td>{{$proj->category->name}}</td>
-                <td>
 
-                    @foreach($proj->users as $key => $user)
-                        <strong>
-                            @if($key++ > 0)
-                                ,
-                            @endif
-                            {{$user->user->first_name . ' ' . $user->user->last_name}}
-                        </strong>
-                    @endforeach
-                </td>
-                <td>
-                    @foreach($proj->admins as $key => $admin)
-                        <strong> {{$admin->admin->userSearch->first_name . ' ' . $admin->admin->userSearch->last_name}} </strong>
-                        @if($key++ > 1)
-                            ,
-                        @endif
-                    @endforeach
-                </td>
-                <td>
-                    @if(isset($proj->admin_entries[0]))
-                        @if(!$proj->admin_entries[0]->active)
-                            Waiting on Output
-                        @else
-                            @if($proj->admin_entries[0]->admin)
-                                Awaiting User Response
-                            @else
-                                <strong> Awaiting Premedia Response </strong>
-                            @endif
-                         @endif
-                        @else
-                            @if(!$proj->active)
-                                Awaiting Initial Upload
-                            @endif
-                    @endif
-
-                </td>
-                <td>
-                    <a class="btn btn-dark" href="/admin/project/{{$proj->file_path}}"><i class="fa fa-arrow-right" aria-hidden="true"></i> Go to project</a>
-                </td>
-            </tr>
+    @foreach($orders as $order)
+        @foreach($order->admins as $admin)
+            @if($admin->admin->user_id == Auth::id())
+                @php
+                    $class = 'belongs';
+                @endphp
+                @break
+            @endif
         @endforeach
-        </tbody>
-    </table>
+        <div class="order {{$class}}">
+            <p class="title">{{$order->job_id}}</p>
+            @if($order->notify_users)
+                <span class="option">Notify Users</span>
+            @endif
+            @if($order->notify_admins)
+                <span class="option">Notify Users</span>
+            @endif
+            @if(!$order->hidden)
+                <span class="option">Public</span>
+            @endif
+            <br class="clear" />
+            <br>
+            @foreach($order->projects as $proj)
+                <a href="{{ url('/admin/project') . '/' . $proj->file_path }}">
+                    <div class="project">
+                        <p class="projectTitle">{{$proj->project_name}}</p>
+                        <p class="statusText">
+                            Status:
+                            <strong>
+                                @if(isset($proj->admin_entries[0]))
+                                    @if(!$proj->admin_entries[0]->active)
+                                        Waiting on Output
+                                    @else
+                                        @if($proj->admin_entries[0]->admin)
+                                            Awaiting User Response
+                                        @else
+                                            <strong> Awaiting Premedia Response </strong>
+                                        @endif
+                                    @endif
+                                @else
+                                    @if(!$proj->active)
+                                        Awaiting Initial Upload
+                                    @endif
+                                @endif
+                            </strong>
+                        </p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    @endforeach
 @endsection
