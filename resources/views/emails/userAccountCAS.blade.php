bdf0cdb98de0af860e3467b2422db4cadfd4b87d
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-16 10:51:42 -0700

    Create CAS new user email

---
+++ resources/views/emails/userAccountCAS.blade.php
@@ -0,0 +1,44 @@
+@include('emails.components.header')
+<table border="0" cellpadding="0" cellspacing="0" class="body">
+    <tr>
+        <td>&nbsp;</td>
+        <td class="container">
+            <div class="content">
+
+                <!-- START CENTERED WHITE CONTAINER -->
+                <span class="preheader">Thank you for registering</span>
+                <table class="main">
+
+                    <!-- START MAIN CONTENT AREA -->
+                    <tr>
+                        <td class="wrapper">
+                            <table border="0" cellpadding="0" cellspacing="0">
+                                <tr>
+                                    <td>
+                                        <h1>Hi {{$user->first_name}}</h1>
+                                        <p>You have been registered as a new customer at the Print and Imaging Lab's proofing system</p>
+                                        <p>Your email is: <strong>{{$user->email}}</strong></p>
+                                        <table border="0" cellpadding="0" cellspacing="0" class="btn btn-primary">
+                                            <tbody>
+                                            <tr>
+                                                <td align="left">
+                                                    <table border="0" cellpadding="0" cellspacing="0">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td> <a href="{{$redirect}}" target="_blank">Login</a> </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <!-- END MAIN CONTENT AREA -->
+                </table>
+@include('emails.components.footer')
