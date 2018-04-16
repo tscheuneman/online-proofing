e59a7b6ad1e7557a52f25ddd3ddf566b926b4457
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-16 10:52:27 -0700

    Create CAS mail handler

---
+++ app/Mail/UserCreatedCAS.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+use App\User;
+
+class UserCreatedCAS extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.userAccountCAS', [
+            'user' => $this->user,
+            'redirect' => '/login?type=admin'
+        ]);
+    }
+}
