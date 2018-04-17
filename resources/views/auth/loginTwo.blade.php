2cc9914332fff9608a5ca8b2e96dd5f98ce5f341
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-16 18:13:59 -0700

    Display error

--- resources/views/auth/loginTwo.blade.php
+++ resources/views/auth/loginTwo.blade.php
@@ -19,12 +19,16 @@
                                 </div>
                             </div>
                             <div class="form-group row">
-                                <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                                <label for="password" class="col-sm-4 col-form-label text-md-right">{{ __('Password') }}</label>
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" autofocus required>
+                                    @if ($errors->has('password'))
+                                        <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                    @endif
                                 </div>
                             </div>
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
