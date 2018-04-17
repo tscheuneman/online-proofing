c62dd96c449daa4262c53dd4abcb92646759209d
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-17 15:24:31 -0700

    Nav active class for all project actions

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -7,7 +7,7 @@
 <nav>
     <a class="{{ Request::is('admin') ? 'active' : '' }}" href="{{ url('/admin') }}"><i class="fa fa-home" aria-hidden="true"></i> home</a>
     <a class="{{ Request::is('admin/customers*') ? 'active' : '' }}" href="{{ url('/admin/customers') }}"><i class="fa fa-users" aria-hidden="true"></i> customers</a>
-    <a class="{{ Request::is('admin/projects*') ? 'active' : '' }}" href="{{ url('/admin/projects') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> projects</a>
+    <a class="{{ Request::is('admin/project*') ? 'active' : '' }}" href="{{ url('/admin/projects') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> projects</a>
     <a class="{{ Request::is('admin/categories*') ? 'active' : '' }}" href="{{ url('/admin/categories') }}"><i class="fa fa-folder-open" aria-hidden="true"></i> categories</a>
     <a class="{{ Request::is('admin/users*') ? 'active' : '' }}" href="{{ url('/admin/users') }}"><i class="fa fa-paint-brush" aria-hidden="true"></i> premedia</a>
 </nav>
