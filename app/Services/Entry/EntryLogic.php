225e48e22cbf648fe79808a228952e3737d3ff1c
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-17 11:06:40 -0700

    Create our EntryLogic service

---
+++ app/Services/Entry/EntryLogic.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services\Entry;
+
+use App\Entry;
+
+class EntryLogic {
+    protected $entry;
+
+    public function __construct(Entry $entry)
+    {
+        $this->entry = $entry;
+    }
+
+    public static function createAdmin($id, $user_id, $path, $comments) {
+        $entry = new Entry();
+        $entry->project_id = $id;
+        $entry->user_id = $user_id;
+        $entry->path = $path;
+        $entry->admin = true;
+        $entry->notes = $comments;
+        $entry->save();
+
+        return new EntryLogic($entry);
+    }
+
+    public function get() {
+        return $this->entry;
+    }
+
+
+}
+
+?>
