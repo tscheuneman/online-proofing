034691668aab11f60e6ae0fe0b3cc2bd84416212
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-19 14:38:13 -0700

    Show all user comments

--- resources/views/admin/projectActions/index.blade.php
+++ resources/views/admin/projectActions/index.blade.php
@@ -92,8 +92,8 @@
                 </div>
                 <div class="card-body hidden revisions">
                     @foreach($project->entries as $enCnt => $entry)
-                        @if($enCnt++ == 0)
-                            @if($project->completed)
+                        @if($project->completed)
+                            @if($enCnt++ == 0)
                                 @if(!empty($project->approval))
                                 <div data-id="{{$project->approval->id}}" class="entryNav wide active approval">
                                         @if($project->approval->user->picture == null)
@@ -218,25 +218,46 @@
                             Comments
                         </div>
                         <div class="card-body">
-                            
-                            @if(!$project->entries[0]->admin )
-                                @if($project->entries[0]->path == '0')
-                                    <div class="comments">
-                                        {{$project->entries[0]->notes}}
-                                    </div>
-                                @else
-                                    @foreach(json_decode($project->entries[0]->user_notes) as $key => $note)
-                                        @if($note != '')
-                                            <div data-num="{{$key}}" class="pageComment">
-                                                <strong>Page {{$key + 1}}</strong>
-                                                <br>
-                                                {{$note}}
+                            @foreach($project->entries as $cmtCnt => $entry)
+                                @if($project->completed)
+                                    @if($cmtCnt++ == 0)
+                                        @if(!empty($project->approval))
+                                            <div data-id="{{$project->approval->id}}" class="commentContainer active cmt_{{$cmtCnt}}">
+                                                <div class="comments">
+                                                    Approved!
+                                                </div>
                                             </div>
                                         @endif
-                                    @endforeach
+                                    @endif
                                 @endif
-                            @endif
-
+                                @if($cmtCnt++ == 0)
+                                    <div data-id="{{$entry->id}}" class="commentContainer active cmt_{{$cmtCnt}}">
+                                @else
+                                    <div data-id="{{$entry->id}}" class="commentContainer cmt_{{$cmtCnt}}">
+                                @endif
+                                @if(!$entry->admin)
+                                    @if($entry->path == '0')
+                                        <div class="comments">
+                                            {{$entry->notes}}
+                                        </div>
+                                    @else
+                                        @foreach(json_decode($entry->user_notes) as $key => $note)
+                                            @if($note != '')
+                                                <div data-num="{{$key}}" class="pageComment">
+                                                    <strong>Page {{$key + 1}}</strong>
+                                                    <br>
+                                                    {{$note}}
+                                                </div>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                @else
+                                    <div class="comments">
+                                        {{$entry->notes}}
+                                    </div>
+                                @endif
+                                </div>
+                            @endforeach
                         </div>
                     </div>
 
