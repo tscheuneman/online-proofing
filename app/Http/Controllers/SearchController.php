9751c24c38a324969e1afcaff81f7ad38ad22118
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-19 12:28:38 -0700

    Revis search controller
    Only allow unique

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -21,6 +21,9 @@ class SearchController extends Controller
 
     public function findProjects(Request $request)
     {
-        return Project::with('order_name.admins.admin.search_user', 'order_name.users.search_user', 'inital_image')->search($request->get('q'))->get(['id', 'file_path', 'ord_id', 'project_name', 'created_at', 'active', 'completed'])->take(10);
+        return Project::with('order_name.admins.admin.search_user', 'order_name.users.search_user', 'inital_image')
+            ->search($request->get('q'))->get(['id', 'file_path', 'ord_id', 'project_name', 'created_at', 'active', 'completed'])
+            ->unique()
+            ->take(5);
     }
 }
