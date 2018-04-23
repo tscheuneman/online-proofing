bfe289c023a75fb6499b46b72d9461aea3d8d47c
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-23 13:56:20 -0700

    Add user relation

--- app/Activity.php
+++ app/Activity.php
@@ -14,4 +14,9 @@ class Activity extends Model
     {
         return $this->belongsTo('App\Project', 'project_id')->select('id', 'project_name', 'ord_id');
     }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id')->select('id', 'first_name', 'last_name');
+    }
 }
