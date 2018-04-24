ee204d0d4f5adbc1bb37cbbb907dcc4e32854b7f
Thomas Scheuneman <thomas@tswebvisions.com>
2018-04-24 12:38:12 -0700

    Display message if no items require attention

--- resources/views/main/index/includes/attentionRequired.blade.php
+++ resources/views/main/index/includes/attentionRequired.blade.php
@@ -1,13 +1,21 @@
 <ul>
+    @php
+        $key = 0;
+    @endphp
     @foreach(json_decode($orders) as $order)
         @foreach($order->projects as $proj)
             @if(isset($proj->admin_entries[0]))
                 @if($proj->admin_entries[0]->admin)
                     <li>
-                        <a class="pendingLink" href="{{url('/project') . '/' . $proj->file_path}}">{{$order->job_id}} - {{$proj->project_name}}</a>
+                        <a class="linkNum_{{$key++}} pendingLink" href="{{url('/project') . '/' . $proj->file_path}}">{{$order->job_id}} - {{$proj->project_name}}</a>
                     </li>
                 @endif
             @endif
         @endforeach
     @endforeach
 </ul>
+@if($key == 0)
+    <strong>
+        Nothing Pending
+    </strong>
+@endif
