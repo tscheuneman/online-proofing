23e49961c2422975c3a200a448abfd2a9425b0b7
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-01 15:58:03 -0700

    Create some guest components.
    These don't allow certain use options.
    These can also be reused for the admin side

---
+++ resources/assets/js/components/AdminGuest/ProofEntryGuest.vue
@@ -0,0 +1,80 @@
+<template>
+    <div v-bind:class="[{ isActive: isActive }, {isAdmin: adminLast}, 'proof-entry proof_' + proofEntry]">
+
+        <template v-if="isFileUpload">
+            <div class="fileUpload">
+                <h5 class="title">{{entry.m.user.first_name + ' ' + entry.m.user.last_name}} uploaded {{numberOfFiles}} file(s)</h5>
+                <small>Upload On {{dateString}}</small>
+            </div>
+        </template>
+
+        <template v-else>
+            <imageEntryGuest
+                    :key="i"
+                    v-for="(z,i) in images"
+                    :image="{z}"
+                    :initalVal = initalValue
+                    :linkVal = linkVal
+                    :keyValue="i"
+                    :entry="entry"
+                    :proofVal="proofEntry"
+            >
+            </imageEntryGuest>
+        </template>
+    </div>
+</template>
+
+<script>
+    import { store } from '../store';
+    export default {
+        name: "proof-entry-guest",
+        props: {
+            entry: Object,
+            proofEntry: Number
+        },
+        data() {
+            return {
+                images: [],
+                initalValue: false,
+                linkVal: null,
+                isActive:false,
+                adminLast: false,
+                isFileUpload: false,
+                numberOfFiles: 0,
+                dateString: null
+            }
+        },
+        mounted() {
+            let self = this;
+            if(self.proofEntry === store.state.currentProof) {
+                self.isActive = true;
+            }
+            self.numberOfFiles = JSON.parse(self.entry.m.files).length;
+        },
+        created() {
+            let self = this;
+            let date = moment(store.state.project.created_at);
+
+            if(this.entry.m.id === store.state.entries[0].id && this.entry.m.admin) {
+                self.adminLast = true;
+                store.state.needResponse = true;
+            }
+            if(self.entry.m.path === null) {
+                let theData = moment(self.entry.m.created_at);
+                self.dateString = theData.format('MMMM Do YYYY, h:mm:ss a');
+
+                self.isFileUpload = true;
+            }
+            else {
+                self.images = JSON.parse(this.entry.m.files);
+                self.initalValue = true;
+                self.linkVal = 'http://localhost:8000/storage/projects/' + date.format('YYYY') + '/' + date.format('MMMM') + '/' + store.state.project.file_path + '/' + this.entry.m.path + '/images';
+            }
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/assets/js/components/AdminGuest/ProofGuest.vue
@@ -0,0 +1,101 @@
+<template>
+    <div id="proof">
+        <proofEntryGuest
+                :key="i"
+                v-for="(m,i) in $store.state.entries"
+                :clickable="true"
+                :entry="{m}"
+                :proofEntry="i"
+        />
+    </div>
+</template>
+
+<script>
+    import { store } from '../store';
+    export default {
+        props: {
+            project: String,
+            user: String
+        },
+        data () {
+            return {
+
+            }
+        },
+        mounted() {
+            let project_id = this.project;
+            let self = this;
+            axios.get('/info/project/'+project_id)
+                .then(function (response) {
+                    let returnData = response.data;
+                    store.commit('addProject', returnData);
+                    store.commit('addEntries', returnData.entries);
+                    self.loadInEntries();
+                })
+                .catch(function (error) {
+                    alert("Failed to initialize cart");
+                    console.log(error);
+                });
+        },
+        created() {
+            Vue.bus.on('goToEntry', function(elm) {
+                if(elm === store.state.currentElm) {
+                    alert('That is the current page');
+                    return false;
+                }
+                $('.proof-entry.isActive .isActive').fadeOut(250, function() {
+                    $(this).removeClass('isActive');
+                    $('.proof-entry.isActive .elem_'+elm).fadeIn(250, function() {
+                        $(this).addClass('isActive');
+                        store.commit('changeActiveElm', elm);
+                    });
+                    $('.thumbnailImage').removeClass('isActive');
+                    $('.thumb_'+elm).addClass('isActive');
+                });
+
+            });
+
+            Vue.bus.on('goToRevision', function(elm) {
+                if(elm === store.state.currentProof) {
+                    alert('That is the current revision');
+                    return false;
+                }
+
+                $('.proof-entry.isActive').fadeOut(250, function() {
+                    $(this).removeClass('isActive');
+                    $('.proof-entry.proof_'+elm).fadeIn(250, function() {
+                        $(this).addClass('isActive');
+                        store.commit('changeCurrentProof', elm);
+
+                        $('.entry').removeClass('isActive');
+                        $('.entry_'+elm).addClass('isActive');
+
+                        $('.navigation-entry').removeClass('isActive');
+                        $('.nav_'+elm).addClass('isActive');
+
+                        let active_elm = $('.proof-entry.isActive .elem.isActive').data('val');
+
+                        store.commit('changeActiveElm', active_elm);
+
+                        $('.navigation-entry.isActive .thumbnailImage').removeClass('isActive');
+                        $('.navigation-entry.isActive .thumb_'+active_elm).addClass('isActive');
+                    });
+                });
+
+            });
+        },
+        methods: {
+            loadInEntries: function() {
+                Vue.bus.emit('loadEntries');
+            },
+            loadInFirstEntry: function() {
+
+            },
+        },
+        name: "proof-guest"
+    }
+</script>
+
+<style scoped>
+
+</style>
