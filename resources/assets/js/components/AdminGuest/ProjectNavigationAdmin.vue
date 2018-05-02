287f5186de077e9b82a9328677f36d2ae0819b84
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-02 10:27:56 -0700

    Create admin project nav comp

---
+++ resources/assets/js/components/AdminGuest/ProjectNavigationAdmin.vue
@@ -0,0 +1,79 @@
+<template>
+    <div id="projectNavigation">
+        <ul class="navContainer">
+            <li v-tooltip="'See all pages'" v-on:click="showSidePictureNavigation"><i class="fa fa-picture-o" aria-hidden="true"></i></li>
+            <li v-tooltip="'Go Home'" v-on:click="goHome"><i class="fa fa-home" aria-hidden="true"></i></li>
+        </ul>
+
+        <ul class="navContainerRight">
+            <li v-tooltip="'View Messaging'" v-on:click="viewMessaging"><i class="fa fa-comments" aria-hidden="true"></i></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { store } from '../store';
+
+    export default {
+        data () {
+            return {
+            }
+
+        },
+        mounted() {
+            Vue.bus.on('goToEntry', function(elm) {
+                if(elm === store.state.currentElm) {
+                    return false;
+                }
+
+                $('.revision-comment.isActive .isActive').fadeOut(250, function() {
+                    $(this).removeClass('isActive');
+                    $('.revision-comment.isActive .usercomm_'+elm).fadeIn(250, function() {
+                        $(this).addClass('isActive');
+
+                    });
+                });
+
+            });
+
+            Vue.bus.on('goToRevision', function(elm) {
+                if(elm === store.state.currentProof) {
+                    return false;
+                }
+
+                $('.revision-comment').removeClass('isActive');
+                $('.revcom_'+elm).addClass('isActive');
+
+            });
+        },
+        methods: {
+            goHome() {
+                location.assign("/");
+            },
+            showSidePictureNavigation() {
+                if(!$('#pagesLeft').hasClass('active')) {
+                    $('#pagesLeft').animate({
+                        width: '12%'
+                    },300, function() {
+                        $('.pagesLeftContent', this).fadeIn(300);
+                        $(this).addClass('active');
+                    });
+                }
+
+            },
+            viewMessaging() {
+                alert('hi Message');
+            },
+            hideAll(_callback) {
+                $('#revisions, #comments, #messaging').fadeOut(300, function() {
+                    _callback();
+                });
+            }
+        },
+        name: "project-navigation-admin"
+    }
+</script>
+
+<style scoped>
+
+</style>
