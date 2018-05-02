104087fbdcee92beaf61d224fe47f8616fc6b6e1
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-02 16:01:07 -0700

    Create comment entry for admin
    Highlights

---
+++ resources/assets/js/components/AdminGuest/CommentEntryAdmin.vue
@@ -0,0 +1,48 @@
+<template>
+    <div
+            v-bind:class="['commentEntry', 'comm_'+keyEntry]"
+            :style="{
+            height: itemEntry.x.height + 'px',
+            width: itemEntry.x.width + 'px',
+            top: itemEntry.x.startY + 'px',
+            left: itemEntry.x.startX + 'px',
+            background: '#fff url('+link+') no-repeat',
+            backgroundSize: 'initial',
+            backgroundPositionX: '-' + itemEntry.x.startX + 'px',
+            backgroundPositionY: '-' + itemEntry.x.startY + 'px',
+            }"
+    >
+        <div class="commentInfo">
+            {{itemEntry.x.comment}}
+        </div>
+    </div>
+</template>
+
+<script>
+    import { store } from '../store';
+
+    export default {
+        props: {
+            itemEntry: Object,
+            keyEntry: Number,
+            link: String
+        },
+        data () {
+            return {
+                linkVal: null,
+            }
+
+        },
+        mounted() {
+            let self = this;
+        },
+        methods: {
+
+        },
+        name: "comment-image"
+    }
+</script>
+
+<style scoped>
+
+</style>
