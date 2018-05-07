bec970c572ac3c2ec084ef859f079b1f6f8f432a
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-07 13:54:03 -0700

    Add spec link

--- resources/views/layouts/includes/admin/sidebar.blade.php
+++ resources/views/layouts/includes/admin/sidebar.blade.php
@@ -9,5 +9,6 @@
     <a class="{{ Request::is('admin/customers*') ? 'active' : '' }}" href="{{ url('/admin/customers') }}"><i class="fa fa-users" aria-hidden="true"></i> customers</a>
     <a class="{{ Request::is('admin/project*') ? 'active' : '' }}" href="{{ url('/admin/projects') }}"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> projects</a>
     <a class="{{ Request::is('admin/categories*') ? 'active' : '' }}" href="{{ url('/admin/categories') }}"><i class="fa fa-folder-open" aria-hidden="true"></i> categories</a>
+    <a class="{{ Request::is('admin/specification*') ? 'active' : '' }}" href="{{ url('/admin/specifications') }}"><i class="fa fa-tags" aria-hidden="true"></i> specifications</a>
     <a class="{{ Request::is('admin/users*') ? 'active' : '' }}" href="{{ url('/admin/users') }}"><i class="fa fa-paint-brush" aria-hidden="true"></i> premedia</a>
 </nav>
