3bb484e9129d6af186d582bdfa4ac229c4382712
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-07 13:54:36 -0700

    Create request validation

---
+++ app/Http/Requests/SpecificationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SpecificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'spec_name' => 'required|string',
+            'content_type' => 'required|string',
+            'default_value' => 'required|string'
+        ];
+    }
+}
