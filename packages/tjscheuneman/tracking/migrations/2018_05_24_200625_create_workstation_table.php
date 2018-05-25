b046bd7c64ba3e389a8e81f6dc2893ffcfda7078
Thomas Scheuneman <thomas@tswebvisions.com>
2018-05-24 22:22:33 -0700

    Create workstation migration

---
+++ packages/tjscheuneman/tracking/migrations/2018_05_24_200625_create_workstation_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorkstationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('workstations', function (Blueprint $table) {
+            $table->uuid('id')->unique();
+            $table->primary('id');
+
+            $table->string('name')->nullable(false);
+            $table->string('icon')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('workstations');
+    }
+}
