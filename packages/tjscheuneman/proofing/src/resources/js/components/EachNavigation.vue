cd54c7c4c78baedc2b6bd0b60bbb90ba99ea6425
Thomas Scheuneman <thomas@tswebvisions.com>
2018-06-06 12:11:23 -0700

    Redo storage url

--- packages/tjscheuneman/proofing/src/resources/js/components/EachNavigation.vue
+++ packages/tjscheuneman/proofing/src/resources/js/components/EachNavigation.vue
@@ -48,7 +48,7 @@
             else {
                 self.images = JSON.parse(this.entry.m.files);
                 self.initalValue = true;
-                self.linkVal = 'http://localhost:8000/storage/projects/' + date.format('YYYY') + '/' + date.format('MMMM') + '/' + store.state.project.file_path + '/' + this.entry.m.path + '/images';
+                self.linkVal = store.state.currentURL + '/storage/projects/' + date.format('YYYY') + '/' + date.format('MMMM') + '/' + store.state.project.file_path + '/' + this.entry.m.path + '/images';
             }
 
 
